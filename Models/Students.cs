4600e72329e75eff359ae505ccfb3713ed2d1b63
diliniudeshikasenarathna <dilinikariyawasam.kbdu@gmail.com>
2025-11-26 15:24:43 +0530

    ïnitia

---
+++ Models/Students.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class Students
+    {
+         public int id {get;set;}
+        public string name{get;set;}
+        public string email{get;set;}
+        public int age{get;set;}
+    }
+}
